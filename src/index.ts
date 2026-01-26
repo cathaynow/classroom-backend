919185a6ba68e9c8caeb8839db7909ed25fe8d28
Cathay <dodeux81@gmail.com>
2026-01-26 22:57:51 +0900

    implement suggestions from coderabbit

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,10 @@ const port = 5555;
 
 app.use(express.json());
 app.use(cors({
-    origin: 'process.env.FRONTEND_URL',
+    origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
-}))
+}));
 
 app.use('/api/subjects', subjectRouter)
 
